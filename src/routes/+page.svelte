97b77c4a2b6e1f2639f972a83adb47df39ca5f38
xiantong zhao <99571021+zxxt2002@users.noreply.github.com>
2023-04-19 15:07:36 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -105,7 +105,7 @@
 	<div class="pt-4">
 		<h2>Generated Article:</h2>
 		{#if answer}
-		<textarea rows="20" bind:value=<p>{answer}</p> style="width: 100%;"></textarea>
+		<textarea rows="20" bind:value={answer} style="width: 100%;"></textarea>
 		{/if}
 		
 		{#if answer}
