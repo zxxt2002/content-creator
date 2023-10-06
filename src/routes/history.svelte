f0c63125b66441c30c2447acae60ce94c4d88cbf
xiantong zhao <99571021+zxxt2002@users.noreply.github.com>
2023-10-06 08:30:17 -0700

    Create history.svelte

---
+++ src/routes/history.svelte
@@ -0,0 +1,11 @@
+<script>
+    let userHistory = [];
+    // Fetch user history from your backend and populate the userHistory array
+</script>
+
+<h2>Your History</h2>
+<ul>
+    {#each userHistory as historyItem}
+        <li>{historyItem}</li>
+    {/each}
+</ul>
