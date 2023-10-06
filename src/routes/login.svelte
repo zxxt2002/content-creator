5bd06502aec449975f733039a03620032c1ba429
xiantong zhao <99571021+zxxt2002@users.noreply.github.com>
2023-10-06 08:29:00 -0700

    Create login.svelte

---
+++ src/routes/login.svelte
@@ -0,0 +1,20 @@
+<script>
+    let username = '';
+    let password = '';
+
+    const handleLogin = () => {
+        // Handle your login logic here
+        // Likely involves sending username and password to your backend
+    };
+</script>
+
+<h2>Login</h2>
+<form on:submit|preventDefault={handleLogin}>
+    <label for="username">Username:</label>
+    <input type="text" bind:value={username} />
+
+    <label for="password">Password:</label>
+    <input type="password" bind:value={password} />
+
+    <button type="submit">Login</button>
+</form>
