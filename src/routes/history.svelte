b76b6151c69c0ae94b11ec879ce637a5c467a890
xiantong zhao <99571021+zxxt2002@users.noreply.github.com>
2023-10-15 14:00:18 -0700

    Update history.svelte

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -21,14 +21,49 @@
     <button on:click={closeHistoryPopup} class="close-btn">x</button>
     <h2 class="text-3xl font-medium pb-1">History</h2>
     <!-- Here you can list out your history content -->
-    <ul>
-        <!-- Example history list -->
-        <li>Visited: Home Page</li>
-        <li>Visited: About Page</li>
+    <ul class="history-list">
+        <li class="history-item">
+            <span class="icon">📄</span> Visited: Home Page
+        </li>
+        <li class="history-item">
+            <span class="icon">📄</span> Visited: About Page
+        </li>
+        <!-- Add more history items as required -->
     </ul>
 </div>
 {/if}
 
 <style>
     /* Add the same styles for overlay, modal, and close-btn from your login.svelte */
+    .history-list {
+        list-style: none;
+        padding: 0;
+    }
+
+    .history-item {
+        display: flex;
+        align-items: center;
+        padding: 5px 0;
+    }
+
+    .icon {
+        margin-right: 10px;
+        font-size: 1.2rem;
+    }
+
+    .close-btn {
+        background-color: #f0f0f0;
+        border: none;
+        border-radius: 50%;
+        cursor: pointer;
+        font-size: 1.2rem;
+        width: 30px;
+        height: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        top: 10px;
+        right: 10px;
+    }
 </style>
