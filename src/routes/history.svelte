a4c2285ffef84085906a083b21547fde8619b1ed
xiantong zhao <99571021+zxxt2002@users.noreply.github.com>
2023-10-15 13:02:17 -0700

    Update history.svelte

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -2,12 +2,6 @@
     import { createEventDispatcher } from 'svelte';
     const dispatch = createEventDispatcher();
 
-    let isPopupVisible = false;
-
-    const showHistoryPopup = () => {
-        isPopupVisible = true;
-    };
-
     const closeHistoryPopup = () => {
         isPopupVisible = false;
         dispatch('close');  // this will notify any parent component if needed
@@ -25,7 +19,7 @@
 <div class="modal fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded-lg shadow-lg flex flex-col justify-between z-20">
     <button on:click={closeHistory} class="close-btn">x</button>
     <h2 class="text-3xl font-medium pb-1">History</h2>
-    <!-- Here you can list out your history content, whether it's in a list, table, etc. -->
+    <!-- Here you can list out your history content -->
     <ul>
         <!-- Example history list -->
         <li>Visited: Home Page</li>
