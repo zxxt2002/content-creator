879a71f97b6ec2adfe0339e86f57dd1f8888566c
xiantong zhao <99571021+zxxt2002@users.noreply.github.com>
2023-10-15 13:52:17 -0700

    Update history.svelte

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -15,7 +15,7 @@
 
 
 
-{#if isPopupVisible}
+{#if visible}
 <div class="overlay fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 z-10"></div>
 <div class="modal fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded-lg shadow-lg flex flex-col justify-between z-20">
     <button on:click={closeHistoryPopup} class="close-btn">x</button>
