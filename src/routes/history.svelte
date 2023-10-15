b085ae5aca6412c19703a89bfec7ae84001d4561
xiantong zhao <99571021+zxxt2002@users.noreply.github.com>
2023-10-15 12:58:19 -0700

    Update history.svelte

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -12,6 +12,8 @@
         isPopupVisible = false;
         dispatch('close');  // this will notify any parent component if needed
     };
+
+    let isPopupVisible = true;
 </script>
 
 <!-- Your content before the button -->
@@ -19,11 +21,16 @@
 
 
 {#if isPopupVisible}
-<div class="overlay" on:click={closeHistoryPopup}></div>
-<div class="modal">
-    <button on:click={closeHistoryPopup} class="close-btn">x</button>
-    <h2>History</h2>
-    <!-- You can list out your history content here, whether it's in a list, table, etc. -->
+<div class="overlay fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 z-10"></div>
+<div class="modal fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded-lg shadow-lg flex flex-col justify-between z-20">
+    <button on:click={closeHistory} class="close-btn">x</button>
+    <h2 class="text-3xl font-medium pb-1">History</h2>
+    <!-- Here you can list out your history content, whether it's in a list, table, etc. -->
+    <ul>
+        <!-- Example history list -->
+        <li>Visited: Home Page</li>
+        <li>Visited: About Page</li>
+    </ul>
 </div>
 {/if}
 
